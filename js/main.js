5bd4420f3214889e592684dc75bea05d83fbfca2
denden11 <abeniondennis11@gmail.com>
2021-07-12 16:21:09 +0800

    add new page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function GigyaLoadFunctions() 
-{
-    console.log(gigya.accounts.getAccountInfo({"callback":function(res) {
-        if (res.errorCode === 0) {
-            window.userGigyaData = res;
-            }
-    }}));
+function GigyaLoadFunctions() {gigya.accounts.getAccountInfo({"callback":function(res) {
+    if (res.errorCode === 0) {
+        window.userGigyaData = res;
+        }
+    }   
+});
 }
 var onGigyaServiceReady = function() {
-    console.log(GigyaLoadFunctions());
+    GigyaLoadFunctions();
 }
