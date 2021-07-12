e563656e070d3e8449aa5886b39169e6957f465c
denden11 <abeniondennis11@gmail.com>
2021-07-12 16:01:15 +0800

    2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function GigyaLoadFunctions() {gigya.accounts.getAccountInfo({"callback":function(res) {
-    if (res.errorCode === 0) {
-        window.userGigyaData = res;
-        }
-    }   
-});
+function GigyaLoadFunctions() 
+{
+    console.log(gigya.accounts.getAccountInfo({"callback":function(res) {
+        if (res.errorCode === 0) {
+            window.userGigyaData = res;
+            }
+    }}));
 }
 var onGigyaServiceReady = function() {
     console.log(GigyaLoadFunctions());
