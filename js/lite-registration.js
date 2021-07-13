e003524a8c68608844f6e3d89e5ac64d548384e6
denden11 <abeniondennis11@gmail.com>
2021-07-13 09:46:27 +0800

    4

---
+++ js/lite-registration.js
@@ -0,0 +1,36 @@
+function liteRegistrationCallback(res) {
+  var myToken, myEmail, myProfile, callParams;
+  myToken = res.regToken;
+  myEmail = "The user's email address";
+  myProfile = {
+    "email": myEmail
+    //, any additional profile data collected, i.e., firstName
+  }
+  callParams = {
+    "regToken": myToken,
+    "profile": myProfile,
+    "subscriptions": {
+      "jun4th": { // This MUST be active and predefined in your site's schema or setAccountInfo will fail!
+        "email": {
+          "isSubscribed": true, // This tells whether the user is actually subscribed to the subscription, this can be false if the user registers but does not want to receive the newsletter.
+          "tags": [
+            // Any tags you want to associate with this user.
+            // The tags property should be removed if you are not setting tags or any existing data will be deleted.
+          ]
+        }
+      }
+    }
+  };
+  gigya.accounts.setAccountInfo(callParams);
+}
+
+function liteRegStart() {
+  gigya.accounts.initRegistration({
+    isLite: true,
+    callback: function (res) {
+      liteRegistrationCallback(res);
+    }
+  });
+}
+
+export {liteRegStart};
--- js/login.js
+++ js/login.js
@@ -1,9 +1,28 @@
-function showAccInfo(response) {
-  console.log(response);
+import {liteRegStart} from "js/lite-registration.js";
+{
+  enabledProviders: "google"
 }
-var idsParams = {
-  include: 'profile, data',
-  extraProfileFields: 'languages, certifications',
-  callback: showAccInfo
-};
-gigya.ids.getAccountInfo(idsParams);
+function Login() {
+  var params = {
+    "callback": onLogin,
+    "provider": "google",
+    context: 'testLogin'
+  };
+  gigya.socialize.login(params);
+}
+//declare the callback function in your code
+function onLogin(response) {
+  if (response.errorCode == 0) {
+    // this will inject the user name and nickname
+    document.getElementById('divUserName').innerHTML = response.user.nickname;
+    ///set the photo to image src attribute.
+    document.getElementById('imgUserPhoto').src = response.user.photoURL;
+    console.log(response);
+  }
+  else {
+    //handle errors
+    alert("An error has occured!" + '\n' +
+      "Error details: " + response.errorMessage + '\n' +
+      "In method: " + response.operation);
+  }
+}
