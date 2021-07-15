b8ca21603255597d3f43bfc7bd7c8ad37dc3caec
denden11 <abeniondennis11@gmail.com>
2021-07-15 15:32:08 +0800

    add getAccountInfoResponse function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 
 document.getElementById("btnLogin").addEventListener("click", Login);
 
-
 function Login(){
   gigya.accounts.showScreenSet({screenSet:'Default-RegistrationLogin'});
 }
+
+function getAccountInfoResponse(response)
+{
+    if ( response.errorCode == 0 ) {           
+        var profile = response['profile'];
+        var msg = profile['firstName'] + ' is ' + profile['age'] + ' years old';
+        alert(msg);
+    }
+    else {
+        alert('Error :' + response.errorMessage);
+    }   
+}
+ 
+gigya.accounts.getAccountInfo({ callback: getAccountInfoResponse });
