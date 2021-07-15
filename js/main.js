efd7f164db89b801c23f0d98451d6c1eb0543b54
denden11 <abeniondennis11@gmail.com>
2021-07-15 17:32:36 +0800

    3

--- js/main.js
+++ js/main.js
@@ -1,31 +1,38 @@
 
-isLogged;
-document.getElementById("btnLogin").addEventListener("click", Login);
+getAccountInfo();
+document.getElementById("btnLoginLogout").addEventListener("click", LoginLogout);
+var isLogged = false;
 
-function isLogged(){
-  gigya.socialize.addEventHandlers({
-    onLogin:Login
-  });
+function LoginLogout(){
+  if (isLogged) {
+    gigya.accounts.logout({callback:afterLogout});
+  }else {
+    gigya.accounts.showScreenSet({
+      screenSet:'Default-RegistrationLogin',
+      onAfterSubmit:getAccountInfo
+    });
+  }
 }
-function Login(eventObj){
-  console.log(eventObj);
-  // gigya.accounts.showScreenSet({
-  //   screenSet:'Default-RegistrationLogin',
-  //   onAfterSubmit:getAccountInfo
-  // });
+function afterLogout(response) {
+  if ( response.errorCode == 0 ) {
+    alert('User has logged out');
+  }
+  else {
+    alert('Error :' + response.errorMessage);
+  }
 }
 function getAccountInfo(){
   gigya.accounts.getAccountInfo({ callback: getAccountInfoResponse });
 }
 function getAccountInfoResponse(response)
 {
-    if ( response.errorCode == 0 ) {
-        document.getElementById("txtWelcome").innerHTML = "Welcome " + response.profile.firstName;
-        document.getElementById("btnLogin").innerHTML = "Logout";
-        console.log(response);
-    }
-    else {
-        console.log('Error :' + response.errorMessage);
-    }
+  if ( response.errorCode == 0 ) {
+      document.getElementById("txtWelcome").innerHTML = "Welcome " + response.profile.firstName;
+      document.getElementById("btnLogin").innerHTML = "Logout";
+      isLogged = true;
+  }
+  else {
+      console.log('Error :' + response.errorMessage);
+  }
 }
  
