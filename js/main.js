e6f06f2abb9fc6e34be2d755df939bb2498eac1e
denden11 <abeniondennis11@gmail.com>
2021-07-21 10:25:05 +0800

    6

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 var isLogged = false;
-function logCheck() {
-  return isLogged;
-}
-function login(){
-  gigya.accounts.showScreenSet({
-    screenSet:'Default-RegistrationLogin',
-    onAfterSubmit:getAccountInfo
-  });
-}
-function logout() {
-  gigya.accounts.logout({callback:afterLogout});
-}
-
-function afterLogout(response) {
-  if ( response.errorCode == 0 ) {
-    isLogged = false;
+module.export = {
+  logCheck() {
+    return isLogged;
+  },
+  login(){
+    gigya.accounts.showScreenSet({
+      screenSet:'Default-RegistrationLogin',
+      onAfterSubmit:getAccountInfo
+    });
+  },
+  logout() {
+    gigya.accounts.logout({callback:afterLogout});
+  },
+  afterLogout(response) {
+    if ( response.errorCode == 0 ) {
+      isLogged = false;
+    }
+    else {
+      alert('Error :' + response.errorMessage);
+    }
+  },
+  getAccountInfo(callbackFun){
+    gigya.accounts.getAccountInfo({ callback: getAccountInfoResponse });
+  },
+  liteRegistration() {
+    gigya.accounts.showScreenSet({
+      screenSet:'Default-LiteRegistration'
+    });
   }
-  else {
-    alert('Error :' + response.errorMessage);
-  }
 }
-function getAccountInfo(callbackFun){
-  gigya.accounts.getAccountInfo({ callback: getAccountInfoResponse });
-}
-function liteRegistration() {
-  gigya.accounts.showScreenSet({
-    screenSet:'Default-LiteRegistration'
-  });
-}
-
-export * from "./main.js";
