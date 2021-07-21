2d3473f5238f9a785b61a179ee1db29d0eb264ee
denden11 <abeniondennis11@gmail.com>
2021-07-21 14:11:18 +0800

    add onlogout response

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,6 +1,12 @@
 // DISPLAY PROFILE INFO
+gigya.socialize.addEventHandlers({
+  onLogout:onLogoutResponse
+});
 gigya.accounts.getAccountInfo({ callback: displayProfile });
 function displayProfile(res) {
   console.log(res);
   document.getElementById("profilePicture").src = res.profile.photoURL;
+}
+function onLogoutResponse(response) {
+  console.log(response);
 }
