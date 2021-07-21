4338f933b1651fd2f3bc7066d597077961b7fef5
denden11 <abeniondennis11@gmail.com>
2021-07-21 14:34:00 +0800

    add fullname

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,7 +1,6 @@
 // DISPLAY PROFILE INFO
 var homepageURL = "https://denden11.github.io/cdc-test-client1/";
 var noErrorCode = 0;
-var profilePictureID = "profilePicture";
 
 gigya.socialize.addEventHandlers({
   onLogout:onLogoutResponse
@@ -10,11 +9,12 @@ gigya.accounts.getAccountInfo({ callback: displayProfile });
 function displayProfile(response) {
   console.log(response);
   if (response.errorCode == noErrorCode) {
-    document.getElementById(profilePictureID).src = response.profile.photoURL;
+    document.getElementById("profilePicture").src = response.profile.photoURL;
+    document.getElementById("profilePicture").src = response.profile.fullName;
   } else {
     window.location.href = homepageURL;
   }
 }
-function onLogoutResponse(response) {
+function onLogoutResponse() {
   window.location.href = homepageURL;
 }
