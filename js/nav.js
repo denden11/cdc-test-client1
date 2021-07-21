4dbba89d5d59cb7d0596b2d9ffe4d6936a7b82ed
denden11 <abeniondennis11@gmail.com>
2021-07-21 10:55:17 +0800

    4

--- js/nav.js
+++ js/nav.js
@@ -6,6 +6,7 @@ document.getElementById("btnLogout").addEventListener("click", logout(afterLogou
 
 getAccountInfo(getAccountInfoResponse);
 function getAccountInfoResponse(response) {
+  console.log(response);
   if (response.errorCode == 0) {
     document.getElementById("btnLogin").classList.add("d-none");
     document.getElementById("dropdownUsername").classList.remove("d-none");
