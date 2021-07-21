1facd610309135473569d0ca72a3a06da677adfe
denden11 <abeniondennis11@gmail.com>
2021-07-21 14:41:57 +0800

    change id to fullName

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -10,7 +10,7 @@ function displayProfile(response) {
   console.log(response);
   if (response.errorCode == noErrorCode) {
     document.getElementById("profilePicture").src = response.profile.photoURL;
-    document.getElementById("profilePicture").innerHTML = response.profile.firstName +" "+response.profile.lastName;
+    document.getElementById("fullName").innerHTML = response.profile.firstName +" "+response.profile.lastName;
   } else {
     window.location.href = homepageURL;
   }
