e223513f6ca4a704996e550e5b298a5a88cf0f32
denden11 <abeniondennis11@gmail.com>
2021-07-21 15:11:38 +0800

    add onload eventlistener before the js

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,15 +1,16 @@
-
-gigya.socialize.addEventHandlers({
-  onLogin:onLoginResponse,
-  onLogout:onLogoutResponse,
-  onError:onErrorResponse
+window.addEventListener("load", function(){
+  gigya.socialize.addEventHandlers({
+    onLogin:onLoginResponse,
+    onLogout:onLogoutResponse,
+    onError:onErrorResponse
+  });
+  function onLoginResponse(response){
+    document.getElementById("txtWelcome").innerHTML = "Welcome " + response.user.firstName;
+  }
+  function onLogoutResponse() {
+    document.getElementById("txtWelcome").innerHTML = "Welcome";
+  }
+  function onErrorResponse(response) {
+    console.log(response);
+  }
 });
-function onLoginResponse(response){
-  document.getElementById("txtWelcome").innerHTML = "Welcome " + response.user.firstName;
-}
-function onLogoutResponse() {
-  document.getElementById("txtWelcome").innerHTML = "Welcome";
-}
-function onErrorResponse(response) {
-  console.log(response);
-}
--- js/pages/profile.js
+++ js/pages/profile.js
@@ -1,19 +1,21 @@
-// DISPLAY PROFILE INFO
-var homepageURL = "https://denden11.github.io/cdc-test-client1/";
-var noErrorCode = 0;
+window.addEventListener("load", function(){
+  // DISPLAY PROFILE INFO
+  var homepageURL = "https://denden11.github.io/cdc-test-client1/";
+  var noErrorCode = 0;
 
-gigya.socialize.addEventHandlers({
-  onLogout:onLogoutResponse
-});
-gigya.accounts.getAccountInfo({ callback: displayProfile });
-function displayProfile(response) {
-  if (response.errorCode == noErrorCode) {
-    document.getElementById("profilePicture").src = response.profile.photoURL;
-    document.getElementById("fullName").innerHTML = response.profile.firstName +" "+response.profile.lastName;
-  } else {
+  gigya.socialize.addEventHandlers({
+    onLogout:onLogoutResponse
+  });
+  gigya.accounts.getAccountInfo({ callback: displayProfile });
+  function displayProfile(response) {
+    if (response.errorCode == noErrorCode) {
+      document.getElementById("profilePicture").src = response.profile.photoURL;
+      document.getElementById("fullName").innerHTML = response.profile.firstName +" "+response.profile.lastName;
+    } else {
+      window.location.href = homepageURL;
+    }
+  }
+  function onLogoutResponse() {
     window.location.href = homepageURL;
   }
-}
-function onLogoutResponse() {
-  window.location.href = homepageURL;
-}
+});
